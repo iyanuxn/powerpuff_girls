93008f7c042b4b9184ec012cc167c5364defa04f
Iyanu <iyanuxn@gmail.com>
2023-08-22 15:07:00 +0100

    SIte complete 80%

--- src/App.jsx
+++ src/App.jsx
@@ -23,16 +23,21 @@ const powerpuffData = [
 
 const App = () => {
   const [initial, setInitial] = useState("w-1/3");
+  const [initialTextSize, setInitialTextSize] = useState("text-6xl")
   const [selected, setSelected] = useState(null);
-  const [textOpened, setTextOpened] = useState(null);
+  const [initialImageSize, setInitialImageSize] = useState("w-60")
 
   const showDetails = (index) => {
     if (selected === index) {
       setSelected(null);
       setInitial("w-1/3");
+      setInitialTextSize("text-6xl")
+      setInitialImageSize("w-60")
     } else {
       setSelected(index);
       setInitial("w-10");
+      setInitialTextSize("text-lg")
+      setInitialImageSize("w-10")
     }
   };
 
@@ -48,10 +53,13 @@ const App = () => {
           }`}
           onClick={() => showDetails(index)}
         >
-          <img src={item.img} className="absolute w-60" />
+          <img src={item.img} className={`
+          absolute
+          ${selected === index ? "w-60" : initialImageSize}
+          `} />
           <b
             className={`
-          text-white text-6xl
+          text-white ${initialTextSize} transition duration-500 ease-in-out
           ${selected === index ? "mt-72 text-9xl" : ""}
           `}
           >
