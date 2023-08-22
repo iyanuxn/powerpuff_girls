25828bd3d98d408f4df996ff720942c401205d30
Iyanu <iyanuxn@gmail.com>
2023-08-22 13:05:23 +0100

    App setup 50%

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const App = () => {
-  return <div className="bg-green-600 h-screen w-screen py-40 px-16">App</div>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="bg-green-500  flex justify-center items-center h-screen w-1/3 py-40 px-16">
+        1
+      </div>
+      <div className="bg-red-500  flex justify-center items-center h-screen w-1/3 py-40 px-16">
+        1
+      </div>
+      <div className="bg-blue-500  flex justify-center items-center h-screen w-1/3 py-40 px-16">
+        1
+      </div>
+    </div>
+  );
 };
 
 export default App;
