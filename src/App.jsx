243974d27e1ca46551035b1a172e9d2c5abc6b09
Iyanu <iyanuxn@gmail.com>
2023-08-22 12:51:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="bg-green-600 h-screen w-screen py-40 px-16">App</div>;
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import CN from "../assets/cn.svg";
+import { FaRegUser } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="absolute inset-0 h-max px-16 py-16 navbar flex justify-between items-center">
+      <img src={CN} alt="CN" className="w-16 h-16" />
+      <div className="flex justify-between items-center text-white text-lg gap-14">
+        <a
+          href="#"
+          className="nav-link hover:scale-110 transition duration-300 ease-in-out"
+        >
+          Games
+        </a>
+        <a
+          href="#"
+          className="nav-link hover:scale-110 transition duration-300 ease-in-out"
+        >
+          Videos
+        </a>
+        <a
+          href="#"
+          className="nav-link hover:scale-110 transition duration-300 ease-in-out"
+        >
+          App
+        </a>
+        <a
+          href="#"
+          className="nav-link hover:scale-110 transition duration-300 ease-in-out"
+        >
+          Toon Cup
+        </a>
+        <a href="">
+          {" "}
+          <FaRegUser className="text-xl nav-link hover:scale-110 transition duration-300 ease-in-out" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
